80fe8fea76217a29f685eff6efeb7e8c54d05090
Chris McClaskey <christopher.m.mcclaskey@gmail.com>
2012-09-14 12:02:09 -0600

    added stats api functionality

--- com/statsmix/Client.java
+++ com/statsmix/Client.java
@@ -9,8 +9,10 @@ import java.util.List;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -25,7 +27,9 @@ public class Client {
 	public Client(String apiKey){
 		ApiKey = apiKey;
 	}
-
+	
+	
+	// === TRACK === -----------------------------------------------------------------------------------------------
 	public String track(String metricName){
 		return track(metricName, new ArrayList<NameValuePair>(2));
 	}
@@ -47,7 +51,9 @@ public class Client {
 		return request("track", "post",  properties);
 	}
 	
-	//stats functionality is undocumented and in progress. The downloadable jar will be updated when it is finished. 
+	
+	// === STATS === -----------------------------------------------------------------------------------------------
+	// listStats ---------------------------------------------------------------------------------------------------
 	public String listStats(int metricId)
 	{
 		return listStats(metricId, new ArrayList<NameValuePair>(2));
@@ -57,6 +63,89 @@ public class Client {
 		options.add(new BasicNameValuePair("metric_id", Integer.toString(metricId)));
 		return request("stats", "get", options);
 	}
+	// getStat -----------------------------------------------------------------------------------------------------
+	public String getStat(int metricId, int statId)
+	{
+		List<NameValuePair> properties = new ArrayList<NameValuePair>(2);
+		properties.add(new BasicNameValuePair("metric_id", Integer.toString(metricId)));
+		String resource = "stats/" + Integer.toString(statId) + ".xml";
+		return request(resource, "get", properties);
+	}
+	public String getStat(int metricId, String refId)
+	{
+		List<NameValuePair> properties = new ArrayList<NameValuePair>(2);
+		properties.add(new BasicNameValuePair("metric_id", Integer.toString(metricId)));
+		properties.add(new BasicNameValuePair("ref_id", refId));
+		String resource = "stats/" + refId + ".xml";
+		return request(resource, "get", properties);
+	}
+	// createStat --------------------------------------------------------------------------------------------------
+	public String createStat(int metricId)
+	{
+		return createStat(metricId, new ArrayList<NameValuePair>(2));
+	}
+	public String createStat(int metricId, int value)
+	{
+		List<NameValuePair> properties = new ArrayList<NameValuePair>(2);
+		properties.add(new BasicNameValuePair("value", Integer.toString(value)));
+		return createStat(metricId, properties);
+	}
+	public String createStat(int metricId, List<NameValuePair> properties, JSONObject meta)
+	{
+    	properties.add(new BasicNameValuePair("meta", meta.toString()));
+		return createStat(metricId, properties);
+	}
+	public String createStat(int metricId, List<NameValuePair> properties)
+	{
+		properties.add(new BasicNameValuePair("metric_id", Integer.toString(metricId)));
+		return request("stats", "post", properties);
+	}
+	// updateStat -------------------------------------------------------------------------------------------------
+	public String updateStat(int metricId, int statId, int value)
+	{
+		List<NameValuePair> properties = new ArrayList<NameValuePair>(2);
+		properties.add(new BasicNameValuePair("metric_id", Integer.toString(metricId)));
+		properties.add(new BasicNameValuePair("value", Integer.toString(value)));
+		String resource = "stats/" + Integer.toString(statId);
+		return request(resource, "put", properties);
+	}
+	public String updateStat(int metricId, String refId, int value)
+	{
+		List<NameValuePair> properties = new ArrayList<NameValuePair>(2);
+		properties.add(new BasicNameValuePair("metric_id", Integer.toString(metricId)));
+		properties.add(new BasicNameValuePair("ref_id", refId));
+		properties.add(new BasicNameValuePair("value", Integer.toString(value)));
+		String resource = "stats/" + refId;
+		return request(resource, "put", properties);
+	}
+	// deleteStat -------------------------------------------------------------------------------------------------
+	public String deleteStat(int metricId, int statId)
+	{
+		List<NameValuePair> properties = new ArrayList<NameValuePair>(2);
+		properties.add(new BasicNameValuePair("metric_id", Integer.toString(metricId)));
+		String resource = "stats/" + Integer.toString(statId);
+		return request(resource, "delete", properties);
+	}
+	public String deleteStat(int metricId, String refId)
+	{
+		List<NameValuePair> properties = new ArrayList<NameValuePair>(2);
+		properties.add(new BasicNameValuePair("metric_id", Integer.toString(metricId)));
+		properties.add(new BasicNameValuePair("ref_id", refId));
+		String resource = "stats/" + refId;
+		return request(resource, "delete", properties);
+	}
+	
+	// In Dev
+	// === METRICS === ---------------------------------------------------------------------------------------------
+	// getMetric ---------------------------------------------------------------------------------------------------
+	//public String getMetric(){return "todo";}
+	// createMetric ------------------------------------------------------------------------------------------------
+	//public String createMetric(){return "todo";}
+	// updateMetric ------------------------------------------------------------------------------------------------
+	//public String updateMetric(){return "todo";}
+	// deleteMetric ------------------------------------------------------------------------------------------------
+	//public String deleteMetric(){return "todo";}
+	
 	
 	private String request(String resource, String method, List<NameValuePair> parameters){
 		
@@ -73,6 +162,14 @@ public class Client {
 		else if (method == "get") 
 		{
 			req = new HttpGet(url);
+		}
+		else if (method == "put")
+		{
+			req = new HttpPut(url);
+		}
+		else if (method == "delete")
+		{
+			req = new HttpDelete(url);
 		}
 	    
 	    HttpResponse response = null;
